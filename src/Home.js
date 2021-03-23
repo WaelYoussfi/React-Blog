5b599f5f0ddaa29f0fda3a6e47e3604281ba5a01
Wael Youssfi <youssfi.wael1997@gmail.com>
2021-03-23 02:22:22 +0100

    added fetching exception handling

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 
 const Home = () => {
-  const [blog, setBlog] = useState([
-    { title: "CarsWael", body: "Ford cars", Author: "Wael", id: "1" },
-    { title: "CarsLouay", body: "Ford cars", Author: "Louay", id: "2" },
-    { title: "CarsWala", body: "Ford cars", Author: "Wala", id: "3" },
-    { title: "CarsWael", body: "Ford cars", Author: "Wael", id: "4" },
-  ]);
-  const handleDeletePost = (id) => {
-    const newBlog = blog.filter((post) => post.id !== id);
-    setBlog(newBlog);
-  };
+  const [blog, setBlog] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blog")
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("Couldn't fetch data");
+          }
+          return res.json();
+        })
+        .then((dataFetched) => {
+          setBlog(dataFetched);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, []);
+
   return (
     <div className="home">
-      <BlogList
-        blog={blog}
-        title="All posts"
-        handleDeletePost={handleDeletePost}
-      />
+      {error && <div>{error}</div>}
+      {isLoading && <div>Data is loading ...</div>}
+      {blog && <BlogList blog={blog} title="All posts" />}
     </div>
   );
 };
--- src/blogList.js
+++ src/blogList.js
@@ -1,12 +1,11 @@
-const BlogList = ({ blog, title, handleDeletePost }) => {
+const BlogList = ({ blog, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blog.map((post) => (
         <div className="blog-preview" key={post.id}>
           <h2>{post.title}</h2>
-          <p>Written by {post.Author}</p>
-          <button onClick={() => handleDeletePost(post.id)}>Delete Post</button>
+          <p>Written by {post.author}</p>
         </div>
       ))}
     </div>
